b58000d0a9aee8ccf796092495ec0b1a43dc93a7
mikezks <23002661+mikezks@users.noreply.github.com>
2022-11-18 16:23:59 +0100

    feat: redux side-effects load flights

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
@@ -1,6 +1,11 @@
 import { Flight } from '@flight-workspace/flight-lib';
 import { createAction, props } from '@ngrx/store';
 
+export const flightsLoad = createAction(
+  '[FlightBooking] Flight load',
+  props<{ from: string, to: string, urgent: boolean }>()
+);
+
 export const flightsLoaded = createAction(
   '[FlightBooking] Flight loaded',
   props<{ flights: Flight[] }>()
--- apps/flight-app/src/app/flight-booking/+state/flight-booking.effects.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.effects.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
+import { FlightService } from '@flight-workspace/flight-lib';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, concatMap } from 'rxjs/operators';
-import { Observable, EMPTY, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import * as FlightBookingActions from './flight-booking.actions';
-
 
 @Injectable()
 export class FlightBookingEffects {
 
-  /* loadFlightBookings$ = createEffect(() => {
-    return this.actions$.pipe(
+  loadFlightBookings$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(FlightBookingActions.flightsLoad),
+      switchMap(action => this.flightService.find(
+        action.from,
+        action.to,
+        action.urgent
+      )),
+      map(flights => FlightBookingActions.flightsLoaded({ flights }))
+    )
+  );
 
-      ofType(FlightBookingActions.loadFlightBookings),
-      concatMap(() =>
-        EMPTY.pipe(
-          map(data => FlightBookingActions.loadFlightBookingsSuccess({ data })),
-          catchError(error => of(FlightBookingActions.loadFlightBookingsFailure({ error }))))
-      )
-    );
-  }); */
-
-
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private flightService: FlightService) {}
 }
