623bc787e0d7e53c8cc8725a9e715eb92221f31f
mikezks <23002661+mikezks@users.noreply.github.com>
2022-11-18 14:39:16 +0100

    feat: redux setup, read, write flight search

---
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
@@ -0,0 +1,14 @@
+import { Flight } from '@flight-workspace/flight-lib';
+import { createAction, props } from '@ngrx/store';
+
+export const flightsLoaded = createAction(
+  '[FlightBooking] Flight loaded',
+  props<{ flights: Flight[] }>()
+);
+
+/*
+export const loadFlightBookingsFailure = createAction(
+  '[FlightBooking] Load FlightBookings Failure',
+  props<{ error: any }>()
+);
+ */
