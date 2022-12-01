9d03a59cd18f5b5dc12c05877641991ad2c9ea4e
mikezks <23002661+mikezks@users.noreply.github.com>
2022-12-01 16:24:15 +0100

    feat: local state mgmt flight filter

--- apps/flight-app/src/app/flight-booking/flight-filter/flight-filter.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-filter/flight-filter.component.ts
@@ -1,7 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ComponentStore } from '@ngrx/component-store';
 import { FlightFilter } from '../entities/flight-filter';
+
+
+export interface LocalState {
+  filters: FlightFilter[];
+}
+
+export const initialLocalState: LocalState = {
+  filters: []
+};
+
 
 @Component({
   selector: 'flight-filter',
@@ -12,6 +23,9 @@ import { FlightFilter } from '../entities/flight-filter';
   ],
   templateUrl: './flight-filter.component.html',
   styleUrls: ['./flight-filter.component.css'],
+  providers: [
+    ComponentStore
+  ]
 })
 export class FlightFilterComponent {
   @Input() set filter(filter: FlightFilter) {
@@ -26,10 +40,50 @@ export class FlightFilterComponent {
     urgent: [false]
   });
 
-  constructor(private fb: FormBuilder) {
+  selectedFilter = new FormControl(this.filterForm.getRawValue(), { nonNullable: true });
+
+  /**
+   * Updater
+   */
+
+  addFilter = this.localStore.updater(
+    (state, filter: FlightFilter) => ({
+      ...state,
+      filters: [
+        ...state.filters,
+        filter
+      ]
+    })
+  );
+
+  /**
+   * Selectors
+   */
+
+  selectFilters$ = this.localStore.select(
+    // Selectors
+
+    // Projector
+    state => state.filters
+  );
+
+  selectLastFilters$ = this.localStore.select(
+    // Selectors
+    this.selectFilters$,
+    // Projector
+    filters => filters.slice(-1)[0]
+  );
+
+
+  constructor(
+    private fb: FormBuilder,
+    private localStore: ComponentStore<LocalState>) {
+
+    this.localStore.setState(initialLocalState);
   }
 
   search(): void {
+    this.addFilter(this.filterForm.getRawValue());
     this.searchTrigger.next(this.filterForm.getRawValue());
   }
 }
